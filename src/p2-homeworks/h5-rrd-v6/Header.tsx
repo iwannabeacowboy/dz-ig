19f8adcdf039d2b595ae0372a1763281c69d98d1
iwannabeacowboy <katsuraiakov@mail.ru>
2022-05-05 23:44:08 +0300

    HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import s from './Header.module.css'
+import {PATH} from './Pages';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <NavLink
+                className={({isActive}) => isActive ? `${s.active} ${s.link}` : s.link}
+                to={PATH.PRE_JUNIOR}
+            >
+                Pre-Junior
+            </NavLink>
+            <NavLink
+                className={({isActive}) => isActive ? `${s.active} ${s.link}` : s.link}
+                to={PATH.JUNIOR}
+            >
+                Junior
+            </NavLink>
+            <NavLink
+                className={({isActive}) => isActive ? `${s.active} ${s.link}` : s.link}
+                to={PATH.JUNIOR_PLUS}
+            >
+                Junior+
+            </NavLink>
+            <div className={s.block}/>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div id={s.notfound}>
+            <div className={s.notfound}>
+                <div className={s.notfound404}>
+                    <h3>Oops! Page not found</h3>
+                    <h1><span>4</span><span>0</span><span>4</span></h1>
+                </div>
+                <h2>I am sorry, but the page you requested was not found</h2>
+            </div>
         </div>
+
     )
 }
 
