66179f423a18a51a559e150ea672506a704f3db6
iwannabeacowboy <katsuraiakov@mail.ru>
2022-05-15 05:10:15 +0300

    HW6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,7 +11,7 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState<string>('editable-span-value', ''))
     }
 
     return (
@@ -19,15 +20,17 @@ function HW6() {
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.editableSpan}>
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
+                    spanProps={{children: value ? undefined : 'enter text...', className: s.bold}}
                 />
             </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+            <div className={s.buttonsBlock}>
+                <SuperButton onClick={save} className={s.marginRight}>save</SuperButton>
+                <SuperButton onClick={restore}>restore</SuperButton>
+            </div>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
